4694e08a9b328557eca0b38384e95a545cab9c7e
jasmeetSSalh <jasmeetsalh4@gmail.com>
2025-03-10 20:46:16 -0400

    build settings eslint updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
   images: {
     domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com", "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Browser-HZNDOssbyLixIa4lABR27yelWXveQ0.png"],
   },
